e0f026924d6708fa949adaa542367fbfc2c80f45
JaspreetSingh122 <jas69401@gmail.com>
2023-10-08 06:30:02 +0530

    rendering index.html

--- myapp/views.py
+++ myapp/views.py
@@ -3,7 +3,7 @@ from django.http import JsonResponse
 from django.http import HttpRequest,HttpResponse
 # Create your views here.
 def myfunc(request):
-    return HttpResponse('<h1>Login</h1>')
+    return render(request,'index.html')
 
 def myfunc2(request):
     return HttpResponse('<h1>Home</h1>')
